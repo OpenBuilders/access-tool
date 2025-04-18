30ff0e07791c166f59bdbbaac30b3540a67c141a
Dmitriy Shamal <digr98@gmail.com>
2025-04-18 11:00:17 +0300

    test: react version

--- frontend/src/pages/admin/ConditionPage/components/Toncoin/Toncoin.tsx
+++ frontend/src/pages/admin/ConditionPage/components/Toncoin/Toncoin.tsx
@@ -57,7 +57,7 @@ export const Toncoin = ({
 
       setInitialState(updatedConditionState as Partial<Condition>)
     }
-  }, [categories?.length])
+  }, [categories?.length, condition])
 
   if (!categories?.length || !conditionState?.type) return null
 
