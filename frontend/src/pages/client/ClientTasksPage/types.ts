4b7c59676eea17ebaf45e691ec53762397d2d71c
Dmitriy <digr98@gmail.com>
2025-05-13 14:30:19 +0400

    feat: sorting

--- frontend/src/pages/client/ClientTasksPage/types.ts
+++ frontend/src/pages/client/ClientTasksPage/types.ts
@@ -2,6 +2,7 @@ import { Condition } from '@store'
 
 export interface FormattedConditions {
   whitelist: Condition[]
+  externalSource: Condition[]
   orRequired: Condition[]
   available: Condition[]
   notNeeded: Condition[]
