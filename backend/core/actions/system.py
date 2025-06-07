355206d63168bd06213579d19a399b6628d2dbef
Daniił M. <danik4tl@gmail.com>
2025-06-07 15:06:12 +0200

    feat: Add Redis health check to the API health check

--- backend/core/actions/system.py
+++ backend/core/actions/system.py
@@ -2,11 +2,14 @@ from sqlalchemy import text
 
 from api.pos.common import StatusFDO
 from core.actions.base import BaseAction
+from core.services.superredis import RedisService
 
 
 class SystemAction(BaseAction):
     def healthcheck(self) -> StatusFDO:
         self.db_session.execute(text("SELECT 1"))
+        redis_service = RedisService()
+        redis_service.client.ping()
         return StatusFDO(
             status="ok",
             message="Healthcheck OK",
