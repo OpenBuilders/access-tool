1462de450ba6d9d69f2aa7963371515ef3f91731
Dmitriy <digr98@gmail.com>
2025-06-11 00:48:26 +0300

    feat: descriptions

--- frontend/src/common/utils/createConditionDescription.ts
+++ frontend/src/common/utils/createConditionDescription.ts
@@ -4,12 +4,10 @@ export const createConditionDescription = (condition: Condition) => {
   const { type, category, expected, model, pattern, backdrop } = condition
 
   if (type === 'nft_collection') {
-    const secondPart = category
-      ? category
-      : Number(expected) > 1
-        ? 'Items'
-        : 'Item'
-    return `${expected} ${secondPart}`.trim()
+    const secondPart = category ? `, ${category}` : ''
+
+    const countWord = Number(expected) > 1 ? 'Items' : 'Item'
+    return `${expected} ${countWord}${secondPart}`.trim()
   }
 
   if (type === 'sticker_collection') {
@@ -19,7 +17,7 @@ export const createConditionDescription = (condition: Condition) => {
 
   if (type === 'gift_collection') {
     const secondPart = Number(expected) > 1 ? 'Gifts' : 'Gift'
-    const options = [model, pattern, backdrop].filter(Boolean).join(', ')
+    const options = [model, backdrop, pattern].filter(Boolean).join(', ')
     return `${expected} ${secondPart}${options ? `, ${options}` : ''}`.trim()
   }
 
