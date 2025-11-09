71ee55e895662a09724769cc7cbcb4b51e3aa52a
Dmitriy <digr98@gmail.com>
2025-11-09 19:37:27 +0300

    feat: linter, fullscreen, telegram types

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -10,6 +10,7 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
   }
 )
