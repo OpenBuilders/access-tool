ea147d6d8565dfac521ebbfc722426e44fc61c5b
Dmitriy <digr98@gmail.com>
2025-11-14 16:13:23 +0300

    feat: remove fullscreen & return prev auth logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import { useNavigate, useParams } from 'react-router-dom'
 
 import config from '@config'
 import { AuthService } from '@services'
+import { useUser, useUserActions } from '@store'
 import { useAuthQuery } from '@store-new'
 
 import Routes from './Routes'
@@ -18,26 +19,26 @@ function App() {
   const { darkTheme } = useContext(ThemeContext)
   const navigate = useNavigate()
 
-  useAuthQuery()
+  // useAuthQuery()
 
-  // const { authenticateUserAction, fetchUserAction } = useUserActions()
-  // const { isAuthenticated } = useUser()
+  const { authenticateUserAction, fetchUserAction } = useUserActions()
+  const { isAuthenticated } = useUser()
 
-  // const authenticateUser = async () => {
-  //   try {
-  //     await authenticateUserAction()
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
-  // }
+  const authenticateUser = async () => {
+    try {
+      await authenticateUserAction()
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-  // const fetchUser = async () => {
-  //   try {
-  //     await fetchUserAction()
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
-  // }
+  const fetchUser = async () => {
+    try {
+      await fetchUserAction()
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   useEffect(() => {
     document.documentElement.setAttribute(
@@ -48,22 +49,22 @@ function App() {
     if (darkTheme) {
       window.document.documentElement.style.backgroundColor = '#1c1c1e'
       webApp?.setBackgroundColor('#1c1c1e')
-      // webApp?.setHeaderColor('#1c1c1e')
+      webApp?.setHeaderColor('#1c1c1e')
       webApp?.setBottomBarColor('#1c1c1e')
     } else {
       window.document.documentElement.style.backgroundColor = '#EFEFF4'
       webApp?.setBackgroundColor('#EFEFF4')
-      // webApp?.setHeaderColor('#EFEFF4')
+      webApp?.setHeaderColor('#EFEFF4')
       webApp?.setBottomBarColor('#EFEFF4')
     }
 
-    const { isMobile } = checkIsMobile()
+    // const { isMobile } = checkIsMobile()
 
-    if (!isMobile) return
+    // if (!isMobile) return
 
-    webApp?.requestFullscreen()
-    webApp?.lockOrientation()
-    webApp?.disableVerticalSwipes()
+    // webApp?.requestFullscreen()
+    // webApp?.lockOrientation()
+    // webApp?.disableVerticalSwipes()
   }, [darkTheme])
 
   useEffect(() => {
@@ -74,26 +75,26 @@ function App() {
     }
   }, [])
 
-  // useEffect(() => {
-  //   webApp?.disableVerticalSwipes()
-  //   if (location.pathname && !location.pathname.includes('client')) {
-  //     webApp?.expand()
-  //   }
-  // }, [location.pathname])
+  useEffect(() => {
+    webApp?.disableVerticalSwipes()
+    if (location.pathname && !location.pathname.includes('client')) {
+      webApp?.expand()
+    }
+  }, [location.pathname])
 
-  // useEffect(() => {
-  //   if (!isAuthenticated) {
-  //     authenticateUser()
-  //   }
-  // }, [])
+  useEffect(() => {
+    if (!isAuthenticated) {
+      authenticateUser()
+    }
+  }, [])
 
-  // useEffect(() => {
-  //   if (isAuthenticated) {
-  //     fetchUser()
-  //   }
-  // }, [isAuthenticated])
+  useEffect(() => {
+    if (isAuthenticated) {
+      fetchUser()
+    }
+  }, [isAuthenticated])
 
-  if (!AuthService.isAuth()) return null
+  // if (!AuthService.isAuth()) return null
 
   return (
     <TonConnectUIProvider
